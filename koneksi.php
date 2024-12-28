c69e17b6de46c5b495797cf1ce04b7ea4f70a3c5
shaziaaam <shaziahuang@gmail.com>
2024-12-28 20:22:42 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,18 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "webdailyjournal";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+//check connection
+if($conn->connect_error){
+    die("Connection failed :".
+    $conn->connect_error);
+}
+
+?>
